705a7e94dcd533a0bea955df80a660193fd80c47
MarkoNisevic <marko.nisevic@fayebsg.com>
2021-07-31 14:18:21 +0200

    try api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/data', function () {
+
+    $data = [['id' => 4, 'name' => 'serbia'], ['id' => 5, 'name' => 'serbia']];
+
+    return json_encode($data);
+
+
+});
+
